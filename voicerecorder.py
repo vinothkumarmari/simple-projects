81205b6072b75a0976b060c64f633e62354c27d1
vinothkumar <75151074+vinothkumarmari@users.noreply.github.com>
2021-09-13 19:32:46 +0530

    Add files via upload

---
+++ voicerecorder.py
@@ -0,0 +1,11 @@
+import sounddevice
+from scipy.io.wavfile import write
+
+fps = 44100
+duration = 10
+print("Recording..")
+recording = sounddevice.rec(int(duration*fps), samplerate=fps, channels=2)
+sounddevice.wait()
+print("Done!")
+
+write("output.wav", fps, recording)
